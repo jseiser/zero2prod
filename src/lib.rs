61c7b837e9045c46a61461038c56e802a57f5698
Justin S <justinseiser@gmail.com>
2023-04-22 22:07:58 -0400

    format on save

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,3 @@ pub mod startup;
 // use actix_web::dev::Server;
 // use actix_web::{web, App, HttpResponse, HttpServer};
 // use std::net::TcpListener;
-
-
-
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::routes::{health_check, subscribe};
-use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
