e7e74f4837eaaa34b49eef650a0740aea81c0ab5
Justin S <justinseiser@gmail.com>
2023-05-22 16:23:06 -0400

    add telem

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-// use actix_web::dev::Server;
-// use actix_web::{web, App, HttpResponse, HttpServer};
-// use std::net::TcpListener;
+pub mod telemetry;
